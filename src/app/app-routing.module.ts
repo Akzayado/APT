239431055035603c4281a300a598b0395805d7ce
2047584 <2047584@aup.edu.ph>
2020-03-15 16:49:09 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MyWorksComponent } from './my-works/my-works.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'my_works', component: MyWorksComponent},
+  { path: 'about', component: AboutMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
+export const routingComponents = [AboutMeComponent, MyWorksComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { HeroComponent } from './hero/hero.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    HeroComponent,
+    TimelineComponent,
+    FooterComponent,
+    routingComponents,
   ],
   imports: [
     BrowserModule,
